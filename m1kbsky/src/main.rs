faa4ef0490ebd88663c094ff333573136525a0e8
M1K <michael@m1k.me>
2024-10-25 22:40:01 +0100

    commentS

--- m1kbsky/src/main.rs
+++ m1kbsky/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+
+    // ingest wss firehose https://docs.bsky.app/docs/advanced-guides/firehose
+    // filter to begin (accounts >= 10 followers)
+    // place into memgraph
+
+
+    // decide query algos
+    // register feed
+    // access feed
     println!("Hello, world!");
 }
