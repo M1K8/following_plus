69a4f2012f5ad2ce0367798ab567b5881a7ea3c7
M1K <michael@m1k.me>
2024-10-25 22:41:52 +0100

    pop

---
+++ target/debug/deps/m1kbsky-e6cb242270614de4.d
@@ -0,0 +1,5 @@
+/home/m1karch/bsky/m1kbsky/target/debug/deps/libm1kbsky-e6cb242270614de4.rmeta: src/main.rs
+
+/home/m1karch/bsky/m1kbsky/target/debug/deps/m1kbsky-e6cb242270614de4.d: src/main.rs
+
+src/main.rs:
