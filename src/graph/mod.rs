40696a97827e5f8937dfa891017b1b507f3de42d
M1K <michael@m1k.me>
2024-11-28 21:14:06 +0000

    or not

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -183,11 +183,11 @@ impl GraphModel {
             .run(neo4rs::query("CREATE INDEX ON :User(did)"))
             .await?;
         inner
-            .run(neo4rs::query("CREATE INDEX ON :Post(rkey)"))
+            .run(neo4rs::query("CREATE INDEX ON :Post(rkey)")) 
             .await?;
-        inner
-            .run(neo4rs::query("CREATE EDGE INDEX ON :FOLLOWS(rkey)"))
-            .await?;
+        // inner
+        // .run(neo4rs::query("CREATE EDGE INDEX ON :FOLLOWS(rkey)")) 
+        // .await?;
 
         // Set off background job to do whatever cleaning we want
         let write_lock = Arc::new(Mutex::new(()));
