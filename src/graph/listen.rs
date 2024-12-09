077a089be333dc9eab244adf365014a456197b1f
M1K <michael@m1k.me>
2024-12-09 18:23:21 +0000

    tick

--- src/graph/listen.rs
+++ src/graph/listen.rs
@@ -385,7 +385,7 @@ async fn get_follows(
         Ok(f) => f,
         Err(e) => {
             let err_str = format!("{:?}", e);
-            if err_str.contains("missing field `records`") && !err_str.contains("429`") {
+            if err_str.contains("missing field `records`") && !err_str.contains("429") {
                 return Err(Box::new(RecNotFound {}));
             }
             return Err(Box::new(e));
