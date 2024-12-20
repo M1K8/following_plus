b9f51d13f9bfe6e5bbe42f716e3f4379f435af3d
M1K <michael@m1k.me>
2024-12-20 20:07:57 +0000

    increase limit to handle everything

--- src/graph/queries.rs
+++ src/graph/queries.rs
@@ -174,7 +174,8 @@ DETACH DELETE u
 "#;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+/// Sorting by timestamp happens in RustLand, as it seems to be signigicantly faster than in memgraphLand (~2.3s for each query -> 300ms), given that we sort by ts again anyway once the results are combined
+/// 
 pub(crate) const GET_FOLLOWING_PLUS_LIKES: &str = r#"
 MATCH (og:User {did: $did})-[:FOLLOWS]->(:User)-[:FOLLOWS]->(u:User)-[:POSTED]->(p:Post)
 WITH u,p,og
@@ -190,7 +191,7 @@ WHERE p IS NOT NULL AND p.likes >= 75
 WITH p, u, toInteger(p.timestamp) AS ts
 WHERE ts < {}
 
-RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 500
+RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 600
 "#;
 
 pub(crate) const GET_FOLLOWING_PLUS_REPOSTS: &str = r#"
@@ -207,7 +208,7 @@ WITH p, u, toInteger(p.timestamp) AS ts
 
 WHERE ts < {}
 
-RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 500
+RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 600
 "#;
 
 pub(crate) const GET_BEST_2ND_DEG_REPOSTS: &str = r#"
@@ -223,7 +224,7 @@ WITH u, b, p, toInteger(p.timestamp) AS ts, CASE WHEN b IS NULL
   THEN p ELSE NULL END as post
 WHERE post IS NOT NULL AND ts < {}
 
-RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 500
+RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 600
 "#;
 
 pub(crate) const GET_BEST_2ND_DEG_LIKES: &str = r#"
@@ -239,7 +240,7 @@ WITH u, b, p, toInteger(p.timestamp) AS ts,  CASE WHEN b IS NULL
   THEN p ELSE NULL END as post
 WHERE post IS NOT NULL AND ts < {}
 
-RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 500
+RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 600
 "#;
 
 pub(crate) const GET_BEST_FOLLOWED: &str = r#"
@@ -247,5 +248,5 @@ MATCH (og:User {did: $did})-[:FOLLOWS]->(u:User)-[:POSTED]->(p:Post)
 WITH p, u, toInteger(p.timestamp) AS ts
 WHERE (p.likes > 10 OR p.reposts > 5) AND (ts - {}) <= 120000000 // last 2 mins
 
-RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 500
+RETURN u.did AS user, p.rkey AS url, ts ORDER BY ts DESC LIMIT 600
 "#;
